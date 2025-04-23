9b85b90b4584a6857ac72956d56b0b2f7c4d7572
richardjean-pierretevreth.uk <zametrics@gmail.com>
2025-04-23 09:36:48 +0200

    deleted unused code

--- src/main/java/ch/uzh/ifi/hase/soprafs24/entity/Lobby.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/entity/Lobby.java
@@ -1,7 +1,5 @@
 package ch.uzh.ifi.hase.soprafs24.entity;
 
-import ch.uzh.ifi.hase.soprafs24.constant.UserStatus;
-import java.time.LocalDateTime;
 import java.util.*;
 import java.util.stream.Collectors;
 
